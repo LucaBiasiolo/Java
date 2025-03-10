4de44411f0a3e2d2e10d7d68b857029454f7db21
Luca Biasiolo <lcbiasiolo@gmail.com>
2025-03-10 12:05:51 +0100

    Create skeleton of diningreview webapp using spring initialzr

---
+++ diningreview/src/test/java/com/diningreview/DiningreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diningreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningreviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

2b571ae5b2832e4e5dfa915a53aa79897a4f2c69
Luca Biasiolo <lcbiasiolo@gmail.com>
2025-03-18 11:53:28 +0100

    Improve logic using maps

--- esercizi/rockpaperscissors/RockPaperScissors.java
+++ esercizi/rockpaperscissors/RockPaperScissors.java
@@ -1,10 +1,28 @@
 package esercizi.rockpaperscissors;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 
 //This is a simple implementation of Rock-Paper-Scissors game in Java
 public class RockPaperScissors {
+
+    private static String getWordFromLetter(String letter){
+        Map<String, String> mapLetterToWord = new HashMap<>(3);
+        mapLetterToWord.put("r", "Rock");
+        mapLetterToWord.put("p", "Paper");
+        mapLetterToWord.put("s", "Scissors");
+        return mapLetterToWord.get(letter);
+    }
+
+    private static String getLetterFromInteger(Integer number){
+        Map<Integer, String> mapIntegerToWord = new HashMap<>();
+        mapIntegerToWord.put(0, "r");
+        mapIntegerToWord.put(1, "p");
+        mapIntegerToWord.put(2, "s");
+        return mapIntegerToWord.get(number);
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Java Rock-Paper-Scissors game!");
@@ -16,48 +34,26 @@ public class RockPaperScissors {
                 System.out.println("Please insert r, p or s");
                 userPick = scanner.nextLine();
             }
-            switch (userPick) {
-                case "r":
-                    System.out.println("You chose Rock");
-                    break;
-                case "p":
-                    System.out.println("You chose Paper");
-                    break;
-                case "s":
-                    System.out.println("You chose Scissors");
-                    break;
-            }
+
+            System.out.println("You chose " + getWordFromLetter(userPick));
+
             Random random = new Random();
             int randomInt = random.nextInt(0, 3);
-            String computerPick = "";
-            switch (randomInt) {
-                case 0:
-                    computerPick = "r";
-                    System.out.println("Computer chose Rock");
-                    break;
-                case 1:
-                    computerPick = "p";
-                    System.out.println("Computer chose Paper");
-                    break;
-                case 2:
-                    computerPick = "s";
-                    System.out.println("Computer chose Scissors");
-                    break;
-            }
+
+            String computerPick = getLetterFromInteger(randomInt);
+            System.out.println("Computer chose " + getWordFromLetter(computerPick));
+
             if (userPick.equals(computerPick)) {
                 System.out.println("It's a tie!");
-            } else if (userPick.equals("r") && computerPick.equals("p")) {
-                System.out.println("You lost! Rock is beaten by Paper");
-            } else if (userPick.equals("r") && computerPick.equals("s")) {
-                System.out.println("You won! Rock beats Scissors");
-            } else if (userPick.equals("p") && computerPick.equals("r")) {
-                System.out.println("You won! Paper beats Rock");
-            } else if (userPick.equals("p") && computerPick.equals("s")) {
-                System.out.println("You lost! Paper is beaten by scissors");
-            } else if (userPick.equals("s") && computerPick.equals("r")) {
-                System.out.println("You lost! Scissors is beaten by Rock");
-            } else if (userPick.equals("s") && computerPick.equals("p")) {
-                System.out.println("You won! Scissors beats paper");
+            } else if (userPick.equals("r")){
+                if(computerPick.equals("p")) System.out.println("You lost! Rock is beaten by Paper");
+                if(computerPick.equals("s")) System.out.println("You won! Rock beats Scissors");
+            } else if (userPick.equals("p")){
+                if(computerPick.equals("r")) System.out.println("You won! Paper beats Rock");
+                if(computerPick.equals("s")) System.out.println("You lost! Paper is beaten by scissors");
+            } else {
+                if(computerPick.equals("r")) System.out.println("You lost! Scissors is beaten by Rock");
+                if(computerPick.equals("p")) System.out.println("You won! Scissors beats paper");
             }
             System.out.println("Do you wish to play again? y/n");
             String userContinue = scanner.nextLine().toLowerCase();

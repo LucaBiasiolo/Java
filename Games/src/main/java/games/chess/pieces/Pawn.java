8e7845a861fc2e565e82947347180febaa68ef66
Luca Biasiolo <lcbiasiolo@gmail.com>
2025-04-15 14:47:48 +0200

    Fix wrong logic on pawn movement

--- Games/src/main/java/games/chess/pieces/Pawn.java
+++ Games/src/main/java/games/chess/pieces/Pawn.java
@@ -20,14 +20,15 @@ public class Pawn extends ChessPiece {
     public boolean isMoveValid(int startX, int startY, int endX, int endY) {
         // The Pawn can move forward one square, or two squares from its starting position
         // It can also capture diagonally
-        int deltaX = Math.abs(endX - startX);
+        int deltaX = endX - startX;
         int deltaY = Math.abs(endY - startY);
 
         // todo: add capture en-passant
+        // the first case is ordinary movement, the second two movement forward and the third for diagonal capturing
         if (isWhite) {
-            return (deltaX == 1 && deltaY == 0) || (deltaX == 2 && deltaY == 0 && startX == 6);
+            return (deltaX == -1 && deltaY == 0) || (deltaX == -2 && deltaY == 0 && startX == 6) || (deltaX == -1 && deltaY == 1);
         } else {
-            return (deltaX == 1 && deltaY == 0) || (deltaX == 2 && deltaY == 0 && startX == 1);
+            return (deltaX == 1 && deltaY == 0) || (deltaX == 2 && deltaY == 0 && startX == 1 ) || (deltaX == 1 && deltaY == 1);
         }
     }
 
